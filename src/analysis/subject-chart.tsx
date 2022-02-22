ff45543f0da325f00ee5f7bb6ada908e97c7a7c9
Sooyoung <dannylisa67@gmail.com>
2022-02-22 11:44:42 +0900

    Update subject-chart.tsx

--- src/analysis/subject-chart.tsx
+++ src/analysis/subject-chart.tsx
@@ -1,5 +1,6 @@
 import { responseState } from "atoms/response.atom"
 import { subjects_list } from "data/subjects-list.data"
+import { Box, Text } from "materials"
 import { useMemo } from "react"
 import { useRecoilValue } from "recoil"
 import { HorizontalChart } from "./horizontal-chart"
@@ -23,13 +24,20 @@ export const SubjectChart = () => {
         const data_nospace = data.map(({name, response}) => ({name:name.split(' ').join(''), response}))
         return data_nospace;
     },[responses])
-
+    const social = useMemo(() => data.slice(1,10).reduce((a, b) => a+b.response, 0), [data])
+    const science = useMemo(() => data.slice(11).reduce((a, b) => a+b.response, 0), [data])
         
     return (
+        <>
         <HorizontalChart 
             data={data}
             xKey="name"
             yKey="response"
         />
+        <Box marginTop={10}>
+            <Text type="P1" content={`사회탐구: ${social}`} />
+            <Text type="P1" content={`과학탐구: ${science}`} marginLeft={10} />
+        </Box>
+        </>
     )
 }
