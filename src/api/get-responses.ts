795f9246849273bd40f9fc6aafaac554dc89a6f8
Sooyoung <dannylisa67@gmail.com>
2022-02-25 10:58:19 +0900

    Update get-responses.ts

--- src/api/get-responses.ts
+++ src/api/get-responses.ts
@@ -8,9 +8,9 @@ export const get_response_api = async () => {
     const res = querySnapshot
         .docs
         .map(d => d.data() as IResponse)
-        .sort((a, b) => (b.createdAt||0) - (a.createdAt || 0))  // 시간 순으로 정렬
-        // .filter((row, index, arr) => {
-        //     return arr.findIndex(item => item.tel === row.tel) === index;   // 중복 제거
-        // });
+        .sort((a, b) => (b.createdAt||0) - (a.createdAt||0))  // 시간 순으로 정렬
+        .filter((row, index, arr) => {
+            return arr.findIndex(item => item.tel === row.tel) === index;   // 중복 제거
+        });
     return res;
 }
